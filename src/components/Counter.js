6d1b0073846748f0e80ad7267006ad3b3f591973
TinaDelli <cecurrier92@gmail.com>
2019-05-13 16:52:21 -0400

    edited manifest json for favicon error and added in the increment async function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,11 +6,13 @@ class Counter extends Component {
     incrementIfOdd = () => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+       
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setInterval(this.updateIncrement, 1000)
     };
 
     updateIncrement = () => {
@@ -37,12 +39,12 @@ class Counter extends Component {
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
+                <button onClick={this.incrementIfOdd}>
                     Increment if odd
                 </button>
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
